d42c6cb6af3da771b14592d9a9eded007396b22b
Truneva Nataliya <truneva.nataliya@mail.ru>
2020-07-19 22:00:47 +0300

    загрузка данных на сервер

--- js/load.js
+++ js/load.js
@@ -1,28 +1,55 @@
 'use strict';
 (function () {
+  var SUCCESS_STATUS = 200;
+  var TIMEOUT = 10000;
   var URL = 'https://javascript.pages.academy/kekstagram/data';
-  window.load = function (onSuccess, onError) {
+  var loadData = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === SUCCESS_STATUS) {
         onSuccess(xhr.response);
       } else {
         onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
     xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
+      onError('Ошибка соединения');
     });
 
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.timeout = 10000; // 10s
+    xhr.timeout = TIMEOUT; // 10s
 
     xhr.open('GET', URL);
     xhr.send();
+  };
+  var uploadData = function (data, onLoad, onError) {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_STATUS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Ошибка соединения');
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+  window.load = {
+    loadData: loadData,
+    uploadData: uploadData
   };
 })();
 
